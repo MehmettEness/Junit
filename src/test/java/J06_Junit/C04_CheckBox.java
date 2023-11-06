8e711abce2ee311a740836e566732b63f0b1bb9a
Mehmet Enes <mehenar@gmail.com>
2023-11-06 13:34:28 +0300

    junit

---
+++ src/test/java/J06_Junit/C04_CheckBox.java
@@ -0,0 +1,56 @@
+package J06_Junit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C04_CheckBox {
+    WebDriver driver;
+
+
+
+
+    // Verilen web sayfasına gidin.
+    // https://the-internet.herokuapp.com/checkboxes
+    @Before
+    public void setUp() throws Exception {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+
+    }
+
+    @Test
+    public void checkBox() throws InterruptedException {
+        // Checkbox1 ve checkbox2 elementlerini locate edin.
+        WebElement checkBox1 = driver.findElement(By.xpath("(//input[@type='checkbox'])[1]"));
+        WebElement checkBox2 = driver.findElement(By.xpath("(//input[@type='checkbox'])[2]"));
+
+        Thread.sleep(3500);
+
+        // Checkbox1 seçili değilse onay kutusunu tıklayın
+        if (!checkBox1.isSelected()){ //isSelected seçili mi
+            checkBox1.click();
+        }
+        // Checkbox2 seçili değilse onay kutusunu tıklayın
+        if (!checkBox2.isSelected()){
+            checkBox2.click();
+        }
+
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Thread.sleep(3500);
+        driver.close();
+
+    }
+}
